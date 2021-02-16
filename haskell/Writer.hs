18d0d9583ce45cc1d803d0ca9133269c55dc027d
IDragnev <iddragnev@gmail.com>
2021-02-16 19:52:22 +0200

    Implement Writer in Haskell

---
+++ haskell/Writer.hs
@@ -0,0 +1,14 @@
+module Writer where
+
+type Writer a = (a, String)
+
+(>=>) :: (a -> Writer b) -> (b -> Writer c) -> (a -> Writer c)
+m1 >=> m2 = \x ->
+    let (y, s1) = m1 x
+        (z, s2) = m2 y
+    in (z, s1 ++ s2)
+
+return :: a -> Writer a
+return x = (x, "")
+
+fmap f = id >=> (\x -> return (f x))
